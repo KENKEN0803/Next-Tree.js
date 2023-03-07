4ff2f1601a91bfbad6ab66295147574d390085ac
JaeHyunLee <kenken0803kr@gmail.com>
2023-03-07 16:31:27 +0900

    linter setting

--- pages/index.js
+++ pages/index.js
@@ -1,86 +1,87 @@
 import styles from '../styles/Home.module.css';
-import React, {useState} from "react";
-import Head from "next/head";
-import TreeJs from "../component/treeJs";
-import {gltfList} from "../lib/constraint";
+import React, { useState } from 'react';
+import Head from 'next/head';
+import TreeJs from '../component/treeJs';
+import { gltfList } from '../lib/constraint';
 
 export default function Home() {
-    const [gltfUrl, setGltfUrl] = useState(gltfList[0].url);
-    return (
-        <div className={styles.container}>
-            <Head>
-                <title>Create Next App</title>
-                <link rel="icon" href="/favicon.ico"/>
-            </Head>
+  const [gltfUrl, setGltfUrl] = useState(gltfList[0].url);
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Create Next App</title>
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
 
-            <select
-                value={gltfUrl}
-                onChange={(e) => {
-                    setGltfUrl(e.target.value);
-                }
-                }>
-                {gltfList.map((gltf, i) =>
-                    <option value={gltf.url} key={`gltf${i}`}>{gltf.name}</option>
-                )}
-            </select>
+      <select
+        value={gltfUrl}
+        onChange={(e) => {
+          setGltfUrl(e.target.value);
+        }}
+      >
+        {gltfList.map((gltf, i) => (
+          <option value={gltf.url} key={`gltf${i}`}>
+            {gltf.name}
+          </option>
+        ))}
+      </select>
 
-            <TreeJs gltfUrl={gltfUrl} width={600} height={600}/>
+      <TreeJs gltfUrl={gltfUrl} width={600} height={600} />
 
-            <style jsx>{`
-              main {
-                padding: 5rem 0;
-                flex: 1;
-                display: flex;
-                flex-direction: column;
-                justify-content: center;
-                align-items: center;
-              }
+      <style jsx>{`
+        main {
+          padding: 5rem 0;
+          flex: 1;
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          align-items: center;
+        }
 
-              footer {
-                width: 100%;
-                height: 100px;
-                border-top: 1px solid #eaeaea;
-                display: flex;
-                justify-content: center;
-                align-items: center;
-              }
+        footer {
+          width: 100%;
+          height: 100px;
+          border-top: 1px solid #eaeaea;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+        }
 
-              footer img {
-                margin-left: 0.5rem;
-              }
+        footer img {
+          margin-left: 0.5rem;
+        }
 
-              footer a {
-                display: flex;
-                justify-content: center;
-                align-items: center;
-                text-decoration: none;
-                color: inherit;
-              }
+        footer a {
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          text-decoration: none;
+          color: inherit;
+        }
 
-              code {
-                background: #fafafa;
-                border-radius: 5px;
-                padding: 0.75rem;
-                font-size: 1.1rem;
-                font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-                DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
-              }
-            `}</style>
+        code {
+          background: #fafafa;
+          border-radius: 5px;
+          padding: 0.75rem;
+          font-size: 1.1rem;
+          font-family: Menlo, Monaco, Lucida Console, Liberation Mono, DejaVu Sans Mono,
+            Bitstream Vera Sans Mono, Courier New, monospace;
+        }
+      `}</style>
 
-            <style jsx global>{`
-              html,
-              body {
-                padding: 0;
-                margin: 0;
-                font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-                Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-                sans-serif;
-              }
+      <style jsx global>{`
+        html,
+        body {
+          padding: 0;
+          margin: 0;
+          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen, Ubuntu,
+            Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+        }
 
-              * {
-                box-sizing: border-box;
-              }
-            `}</style>
-        </div>
-    )
+        * {
+          box-sizing: border-box;
+        }
+      `}</style>
+    </div>
+  );
 }
